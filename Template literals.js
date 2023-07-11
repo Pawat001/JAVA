6b244001df699b2a6b37246b5c480d153de59eb9
Pawat001 <s650602262109@email.kmutnb.ac.th>
2023-07-11 09:12:06 +0700

    save

---
+++ Template literals.js
@@ -0,0 +1,14 @@
+//Concatenation with template literals
+console.log("\n********Conacatenation with template literaks********\n");
+
+let str1 = "JavaScript";
+let str2 = "fun";
+
+console.log(`I am writting code in ${str1}`);
+console.log(`Formatting in ${str1} is ${str2}!`);
+
+//Expressions in template literals
+console.log("\m********Expressions in template litterals********\n");
+
+let bool1 = true;
+console.log(`The opposite of true is ${!bool1}`);
