08ab1343a1251592ba80b269a6567c3d1dc01189
Pawat001 <s650602262109@email.kmutnb.ac.th>
2023-07-11 11:55:17 +0700

    save

---
+++ Lecture2/Function Assigned to variable.js
@@ -0,0 +1,11 @@
+let plusFive = (number) => {
+    return number + 5;
+};
+//f is assigned the value of plusfive
+let f = plusFive;
+
+console.log(plusFive(3)); // 8
+// Since f has a function value, it can ve invoked.// @algorithm
+import * as a from 'C:\Users\Pawat\.alg\node_modules\algm'
+
+console.log (f(9)); //14
---
+++ Lecture2/frst function.js
@@ -0,0 +1,11 @@
+//1. Function Definittion
+function printHello(name) {
+    console.log("Hello "+name);
+    return name + "hello!";
+}
+
+console.log(printHello.name)
+
+// 2. Function Invocaion 
+let result = printHello("Anirach !");
+console.log(result);
